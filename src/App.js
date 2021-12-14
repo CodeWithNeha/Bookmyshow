dbc87b18b721c34567185cd5cfe2dc578bb09ac7
Neha <codewithneha2020@gmail.com>
2021-12-14 20:03:37 +0530

    Movie Pages

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import { Route } from 'react-router-dom';
 
 //HOC
 import DefaultHOC from "./HOC/Default.HOC";
+import MovieHOC from "./HOC/Movie.HOC";
 
 //Component
 import HomePage from "./pages/Home.page";
-
+import Movie from "./pages/movie.page";
 function App() {
   return (
     <>
      <DefaultHOC path="/" exact component={HomePage} />
+     <MovieHOC path="/movie/:id" exact component={Movie} />
     </>
   );
 }
